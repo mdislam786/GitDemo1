f708a89334e7420d7f51a13d487b41cafad421ef
mdislam786 <mdislam@live.com>
2019-09-30 16:37:42 -0400

    dev branch worked on StudentLoan

--- src/basic/BasicClass.java
+++ src/basic/BasicClass.java
@@ -6,20 +6,28 @@ public class BasicClass {
 		
 		personalLoan();
 		businessLoan();
+		studentLoan();
 
 	}
 
 	public static void personalLoan() {
-		System.out.println("Personal Loan");
+		System.out.println("Personal Loan1");
 		System.out.println("Personal Loan2");
 		System.out.println("Personal Loan3");
 
 	}
 	public static void businessLoan() {
-		System.out.println("Business Loan");
+		System.out.println("Business Loan1");
 		System.out.println("Business Loan2");
 		System.out.println("Business Loan3");
 		System.out.println("Business Loan4");
+
+	}
+	
+	public static void studentLoan() {
+		System.out.println("Student Loan1");
+		System.out.println("Student Loan2");
+		System.out.println("Student Loan3");
 
 	}
 }
