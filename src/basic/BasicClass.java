0d126b90de9facff7889317d764d8c873f52ee3a
mdislam786 <mdislam@live.com>
2019-09-30 15:37:13 -0400

    This is my first commit

---
+++ src/basic/BasicClass.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class BasicClass {
+
+	public static void main(String[] args) {
+		
+		personalLoan();
+		businessLoan();
+
+	}
+
+	public static void personalLoan() {
+		System.out.println("Personal Loan");
+
+	}
+	public static void businessLoan() {
+		System.out.println("Business Loan");
+
+	}
+}
